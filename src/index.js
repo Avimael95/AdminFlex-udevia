165ad05b61152ec4d1985db69143a986d1b260f1
Avimael <ing.avimelgr95@gmail.com>
2020-09-23 18:49:01 -0500

    Se crea el nombre del sitio

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+// require('./styles/main.scss');
+//otra forma de cargar los archivos
+import './styles/main.scss';
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
+import './static/webpack.png';
+
+
+
