9d9562f7c1ab8f72ff158e9a6f04757ff38e9a7a
Avimael <ing.avimaelgr95@gmail.com>
2020-09-28 21:52:24 -0500

    Admin Flex v1.1.0

--- src/index.js
+++ src/index.js
@@ -6,7 +6,32 @@ import '@fortawesome/fontawesome-free/js/fontawesome'
 import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
 import '@fortawesome/fontawesome-free/js/brands'
-import './static/webpack.png';
+
+import './static/Logo.png';
+import './static/Logo.svg';
+import './static/user.jpg';
 
 
+const menuIzquierdo = document.querySelector('.menu-izquierdo');
+
+menuIzquierdo.addEventListener('click', (e)=>{
+    const claseMenu = e.target.classList;
+
+    //Crear variable
+    const pagina = document.querySelector('.pagina'),
+          menuIzq = document.querySelector('.fa-arrow-left'),
+          menuDer = document.querySelector('.fa-arrow-right');
+    if(claseMenu.contains('fa-arrow-left')){
+        menuIzq.style.display = 'none';
+        menuDer.style.display = 'block';
+        pagina.classList.add('no-menu');
+    }
+    else if(claseMenu.contains('fa-arrow-right')){
+        
+        menuIzq.style.display = 'block';
+        menuDer.style.display = 'none';
+        pagina.classList.remove('no-menu');
+        pagina.classList.add('sidebar-ok');
+    }
+})
 
